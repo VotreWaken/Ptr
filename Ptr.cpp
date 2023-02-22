04f8faedc579ff03110f7a63dd1bf5ff6f92dea8
Waken <votrevincentwaken@gmail.com>
2023-02-22 23:01:15 +0200

    Ptr

---
+++ Ptr.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Fraction
+{
+private:
+    int m_numerator = 0;
+    int m_denominator = 1;
+
+public:
+    Fraction(int numerator = 0, int denominator = 1) :
+        m_numerator(numerator), m_denominator(denominator)
+    {
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Fraction& f1)
+    {
+        out << f1.m_numerator << "/" << f1.m_denominator;
+        return out;
+    }
+};
+
+void printFraction(Fraction* ptr)
+{
+    if (ptr)
+        std::cout << *ptr;
+}
+
+int main()
+{
+    auto ptr = std::make_unique<Fraction>(7, 9);
+    printFraction(ptr.get());
+}
